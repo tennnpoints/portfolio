18a572de4c4d8c61e6c0b8b8068a7fa311683400
tennnpoints <tennn.points@gmail.com>
2022-12-21 20:32:10 +0900

    settings.py update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7osi#v-ve_9o1wfkip24bl&ehcm*5pc6drskh)82-b)9p0duyh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
